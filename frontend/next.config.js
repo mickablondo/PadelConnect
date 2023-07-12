8600ba9d982fbe48726209481aa710f8f1f42cf7
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-07-12 21:10:18 +0200

    Divers correctifs pour démarrer le front

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false,
+                net: false,
+                tls: false
+            }
+        }
 
-module.exports = nextConfig
+        return config;
+    }
+}
