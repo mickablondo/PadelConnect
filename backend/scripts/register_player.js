857986d54c54819128eaf2b4a5fe31b9683aefd4
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-07-15 13:17:25 +0200

    [Jeux de données] Enregistrement de 2 joueurs

--- backend/scripts/register_player.js
+++ backend/scripts/register_player.js
@@ -3,11 +3,12 @@ const { ethers } = require('hardhat');
 async function main() {
     let owner, manager;
 
-    [owner, manager, player1] = await ethers.getSigners();
+    [owner, manager, player1, player2] = await ethers.getSigners();
     const myContract = await ethers.getContractAt("PadelConnect", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
 
-    // Registering player
-    await myContract.connect(manager).registerPlayer(10, 'roger', 'federer');
+    // Registering players
+    await myContract.connect(player1).registerPlayer(0, 'roger', 'federer');
+    await myContract.connect(player2).registerPlayer(0, 'rafael', 'nadal');
 }
 
 main()
