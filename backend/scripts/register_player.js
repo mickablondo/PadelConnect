a2277691e1f52bfd4e9f9a821d9d359db3fb9194
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-07-16 11:21:13 +0200

    Correction script suite delete player name

--- backend/scripts/register_player.js
+++ backend/scripts/register_player.js
@@ -1,7 +1,7 @@
 const { ethers } = require('hardhat');
 
 async function main() {
-    let owner, manager;
+    let owner, manager, player1, player2;
 
     [owner, manager, player1, player2] = await ethers.getSigners();
     const myContract = await ethers.getContractAt("PadelConnect", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
