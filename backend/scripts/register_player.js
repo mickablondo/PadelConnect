96844aecd70c83200fce894970b43a9d6592a693
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-07-16 11:19:47 +0200

    Correction script suite suppression noms player

--- backend/scripts/register_player.js
+++ backend/scripts/register_player.js
@@ -7,8 +7,8 @@ async function main() {
     const myContract = await ethers.getContractAt("PadelConnect", "0x5FbDB2315678afecb367f032d93F642f64180aa3");
 
     // Registering players
-    await myContract.connect(player1).registerPlayer(0, 'roger', 'federer');
-    await myContract.connect(player2).registerPlayer(0, 'rafael', 'nadal');
+    await myContract.connect(player1).registerPlayer(0);
+    await myContract.connect(player2).registerPlayer(0);
 
     // Adding comments
     await myContract.connect(manager).addComment(0, 'Bienvenue sur le tournoi');
